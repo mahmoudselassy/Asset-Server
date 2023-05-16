901773da28859fea4bc4b6167f405ac091004fdd
Mahmoud Elassy <mahmoudsameh7331@yahoo.com>
2023-05-16 18:08:25 +0200

    add Bard Server

--- Classes/Bard.js
+++ Classes/Bard.js
@@ -1,9 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Bard = void 0;
+const node_fetch_1 = __importDefault(require("node-fetch"));
 class Bard {
-    static sendMessage(asset) {
-        console.log(JSON.stringify(asset));
+    static async sendMessage(asset) {
+        const response = (0, node_fetch_1.default)("localhost:8000", { method: "post", body: JSON.stringify(asset), headers: { "Content-Type": "application/json" } });
     }
 }
 exports.Bard = Bard;
--- Classes/Bard.ts
+++ Classes/Bard.ts
@@ -1,8 +1,9 @@
 import { Asset } from "../Interfaces";
-
+import fetch from "node-fetch";
 class Bard {
-  static sendMessage(asset: Asset) {
-    console.log(JSON.stringify(asset));
+  static async sendMessage(asset: Asset) {
+    const response = fetch("localhost:8000", { method: "post", body: JSON.stringify(asset), headers: { "Content-Type": "application/json" } });
+    
   }
 }
 export { Bard };
